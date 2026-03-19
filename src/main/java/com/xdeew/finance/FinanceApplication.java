4d3a7782175db8d12daa19162286a4a9e22631f9
xDeew <andretrolezi14@gmail.com>
2026-03-19 11:53:48 +0100

    Load local configuration from .env file

--- src/main/java/com/xdeew/finance/FinanceApplication.java
+++ src/main/java/com/xdeew/finance/FinanceApplication.java
@@ -3,11 +3,17 @@ package com.xdeew.finance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FinanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv.configure()
+                .ignoreIfMissing()
+                .systemProperties()
+                .load();
 
+        SpringApplication.run(FinanceApplication.class, args);
+    }
 }
